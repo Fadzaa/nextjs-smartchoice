420c85172cbfdada90559d2f4b297fe7bf69641c
Fadzaa <fadza20@gmail.com>
2024-01-31 02:41:54 +0700

    style: [FA] add hover in list partai card

--- src/app/(pages)/partai/components/partai-card.jsx
+++ src/app/(pages)/partai/components/partai-card.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Image from "next/image";
 
-function PartaiCard({number, image, name}) {
+function PartaiCard({number, image, party_name, description}) {
     return (
-        <div className="flex flex-col justify-between items-center h-[395px] p-6 w-full border border-black border-opacity-10 rounded-2xl shadow-md hover:shadow-lg hover:transition">
+        <div className="
+        flex flex-col justify-between items-center h-[395px] pt-6 w-full overflow-hidden border border-black border-opacity-10 rounded-2xl shadow-md
+        hover:shadow-lg hover:transition-all duration-1000 ease-out group hover:relative hover:pt-0 hover:justify-center
+        ">
             <h3 className="text-primary">No {number}</h3>
             <Image
                 src={image}
-                alt={name}
+                alt={party_name}
                 width={200}
                 height={200}
             />
-            <h3 className="text-[#4D4D4D] text-[20px] font-semibold text-center">{name}</h3>
+            <div className="
+            bg-[#FBFBFB] bg-opacity-95 w-full h-[80px]
+            group-hover:items-center group-hover:justify-start
+            group-hover:h-full group-hover:absolute flex flex-col items-center justify-center p-6">
+                <h3 className="text-[#4D4D4D] text-[20px] font-semibold text-center">{party_name}</h3>
+                <p className="text-[#4D4D4D] text-sm hidden group-hover:block text-center">{description}</p>
+
+            </div>
         </div>
     );
 }
