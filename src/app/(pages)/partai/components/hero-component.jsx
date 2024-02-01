1bd7ddcbcf1b58d6f317a903b632c11cc6e7c2ed
Yasa Kafi Razzan <114861219+YasaKafi@users.noreply.github.com>
2024-02-01 10:35:44 +0700

    feat: [YK] add accordion detail candidate

--- src/app/(pages)/partai/components/hero-component.jsx
+++ src/app/(pages)/partai/components/hero-component.jsx
@@ -5,7 +5,7 @@ export default async function HeroComponent() {
   return (
     <div className=" h-[30vh] mt-16 w-full mb-16">
       <div className="w-full h-full flex gap-6 justify-center flex-row ">
-        <Image src={humanSearching} alt="human-searching" className="w-60" />
+        <Image src={humanSearching} width={10000} height={10000} quality={100} alt="human-searching" className="w-60" />
         <div className=" w-3/5 flex pr-12 gap-3 flex-col text-black">
           <h1 className="text-[50px] font-semibold">
             Kenali Track Record <br/> <span className="text-primary">Partai</span>
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
-const montserrat = Poppins({
+const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
   subsets: ["latin-ext"],
 });
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body className={poppins.className}>
         <Navbar />
 
         {children}
